a89703e30d69d00e7b39becd3732b16aca8a8368
Lippo <filippo.nardini@studio.unibo.it>
2020-03-25 17:02:18 +0100

    Update emacs configuration!

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,91 +2,16 @@
 ;;; Commentary:
 (require 'org)
 ;;; Code:
+(setq vc-follow-symlinks nil)
 (org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline success warning error])
- '(ansi-color-names-vector
-   ["#000000" "#ff8059" "#44bc44" "#eecc00" "#33beff" "#feacd0" "#00d3d0" "#ffffff"])
- '(beacon-color "#c82829")
- '(custom-enabled-themes (quote (modus-vivendi)))
- '(custom-safe-themes
-   (quote
-    ("e3b3e31f9b081f42a619161333c73ae8fb635326c852c098a1c2bd9b967efe8d" "99e48a01fa9e0cd19db5b13f2ba2b091c00341b579d358d5bc32b124c1af5440" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "8e797edd9fa9afec181efbfeeebf96aeafbd11b69c4c85fa229bb5b9f7f7e66c" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
- '(display-battery-mode nil)
- '(display-time-mode nil)
- '(fci-rule-color "#d6d6d6")
- '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
- '(frame-background-mode (quote light))
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(hl-todo-keyword-faces
-   (quote
-    (("HOLD" . "#e5f040")
-     ("TODO" . "#feacd0")
-     ("NEXT" . "#b6a0ff")
-     ("THEM" . "#ed92f8")
-     ("PROG" . "#00d3d0")
-     ("OKAY" . "#4ae8fc")
-     ("DONT" . "#58dd13")
-     ("FAIL" . "#ff8059")
-     ("DONE" . "#44bc44")
-     ("NOTE" . "#f0ce43")
-     ("KLUDGE" . "#eecc00")
-     ("HACK" . "#eecc00")
-     ("TEMP" . "#ffcccc")
-     ("FIXME" . "#ff9977")
-     ("XXX+" . "#f4923b")
-     ("REVIEW" . "#6ae4b9")
-     ("DEPRECATED" . "#aaeeee"))))
- '(ibuffer-deletion-face (quote dired-flagged))
- '(ibuffer-marked-face (quote dired-marked))
  '(package-selected-packages
    (quote
-    (modus-vivendi-theme modus-operandi-theme org-journal org-capture yaml-mode lsp-treemacs posframe sbt-mode scala-mode nodejs-repl indium centered-window web-mode diminish haskell-mode magit exwm-firefox-core exwm javadoc-lookup expand-region counsel-projectile all-the-icons-dired malyon multiple-cursors shell-pop dimmer json-mode tide npm-mode free-keys doom-modeline org-noter pdf-tools elisp-format god-mode htmlize ivy-hydra rainbow-delimiters rainbow-mode company-jedi elpy org-bullets paredit smex counsel kotlin-mode typescript-mode evil-org auctex dap-mode lsp-java company-lsp lsp-ui lsp-mode flycheck projectile evil-collection company evil yasnippet ivy dashboard gruvbox-theme which-key exec-path-from-shell use-package)))
- '(pdf-view-midnight-colors (quote ("#282828" . "#f2e5bc")))
- '(shell-pop-full-span t)
- '(shell-pop-shell-type
-   (quote
-    ("ansi-term" "*ansi-term*"
-     (lambda nil
-       (ansi-term shell-pop-term-shell)))))
- '(shell-pop-term-shell "/bin/zsh")
- '(shell-pop-universal-key "C-t")
- '(shell-pop-window-position "bottom")
- '(shell-pop-window-size 30)
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#c82829")
-     (40 . "#f5871f")
-     (60 . "#eab700")
-     (80 . "#718c00")
-     (100 . "#3e999f")
-     (120 . "#4271ae")
-     (140 . "#8959a8")
-     (160 . "#c82829")
-     (180 . "#f5871f")
-     (200 . "#eab700")
-     (220 . "#718c00")
-     (240 . "#3e999f")
-     (260 . "#4271ae")
-     (280 . "#8959a8")
-     (300 . "#c82829")
-     (320 . "#f5871f")
-     (340 . "#eab700")
-     (360 . "#718c00"))))
- '(vc-annotate-very-old-color nil)
- '(window-divider-mode t)
- '(xterm-color-names
-   ["#000000" "#ff8059" "#44bc44" "#eecc00" "#33beff" "#feacd0" "#00d3d0" "#a8a8a8"])
- '(xterm-color-names-bright
-   ["#181a20" "#f4923b" "#58dd13" "#e5f040" "#72a4ff" "#ed92f8" "#4ae8fc" "#ffffff"]))
+    (dap-mode posframe company-lsp lsp-ui lsp-mode sbt-mode scala-mode tide js2-mode dante company-jedi rainbow-delimiters paredit flycheck company yasnippet ivy-hydra dashboard expand-region org-noter pdf-tools org-journal org-bullets magit counsel-projectile projectile diminish which-key modus-vivendi-theme modus-operandi-theme exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
