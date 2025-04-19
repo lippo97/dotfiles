cff3fce565c0461b05ba7a19ceb68a20c01646d7
Filippo Nardini <filippo.nardini3@unibo.it>
2025-04-19 14:51:02 +0200

    nvim: use default lsp bindings

--- nvim/.config/nvim/lua/lippo/plugins/lsp.lua
+++ nvim/.config/nvim/lua/lippo/plugins/lsp.lua
@@ -21,17 +21,19 @@ return {
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
         vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-
-        vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
-        vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
-
+        -- try to use defaults for these
+        -- K - vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+        -- grr - vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+        -- grn - vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+        -- gra - vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
         vim.diagnostic.config({
           virtual_text = true,
         })
+        vim.keymap.set("n", "[e", function() vim.diagnostic.jump({ count = -1, float = true }) end, { buffer = 0 })
+        vim.keymap.set("n", "]e", function() vim.diagnostic.jump({ count = 1, float = true }) end, { buffer = 0 })
+
       end
     })
   end
